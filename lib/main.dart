9b10ea350adafd3dce9f96f5ce5a4a11fef1dbf9
Bora Korpe <borakorpee@gmail.com>
2022-10-20 20:03:49 +0300

    provider & model

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/places_list_screen.dart';
+import '../providers/place_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(),
+    return ChangeNotifierProvider.value(
+      value: PlaceProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
       ),
     );
   }
---
+++ lib/models/place.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+
+class PlaceLocation {
+  final double lat;
+  final double long;
+
+  PlaceLocation({
+    @required this.lat,
+    @required this.long,
+  });
+}
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    @required this.id,
+    @required this.title,
+    @required this.image,
+    @required this.location,
+  });
+}
